e38ea370f50f4b6e3fbbd859556c0e004cc76daf
KolomiAnna <115336722+KolomiAnna@users.noreply.github.com>
2023-05-31 10:24:19 +0300

    correction props

--- src/components/contactForm/ContactForm.jsx
+++ src/components/contactForm/ContactForm.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import shortid from 'shortid'
+import PropTypes from "prop-types";
 
 import css from 'components/contactForm/contactForm.module.css'
 
@@ -36,7 +37,7 @@ class ContactForm extends React.Component {
                         name="name"
                         value={this.state.name}
                         onChange={this.handleChange}
-                        // pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
                         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
                         required
                         id={this.nameInputId}
@@ -47,8 +48,8 @@ class ContactForm extends React.Component {
                         name="number"
                         value={this.state.number}
                         onChange={this.handleChange}
-                        // pattern="\+?\d{1,4}?[-.\s]?\(?\d{1
-                        //     ,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1
+                            ,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
                         title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
                         required
                         id={this.numberInputId}
@@ -62,7 +63,9 @@ class ContactForm extends React.Component {
 export default ContactForm;
 
 
-
+ContactForm.propTypes = {
+    onSubmit: PropTypes.func.isRequired,
+};
 
 
 
